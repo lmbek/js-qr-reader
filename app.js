066ce78f397a97e797c9b44d8b756810555385f0
Lars Morten Bek <lars@beksoft.dk>
2023-04-29 12:53:46 +0200

    Adding First Prototype * JavaScript QR Reader that does not show image in browser while scanning for QR from camera

---
+++ app.js
@@ -0,0 +1,50 @@
+try {
+    if(jsQR){
+        console.log("it exists")
+    }
+} catch(error) {
+    console.error("we need jsQR.js for our app.js to function (dependency)")
+}
+
+const video = document.createElement("video");
+const canvasElement = document.getElementById("canvas");
+const canvas = canvasElement.getContext("2d");
+const startButton = document.getElementById("startButton");
+const stopButton = document.getElementById("stopButton");
+const resultContainer = document.getElementById("result");
+
+function startScanner() {
+    navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).then((stream) => {
+        video.srcObject = stream;
+        video.setAttribute("playsinline", true);
+        video.play();
+        requestAnimationFrame(tick);
+    });
+}
+
+function stopScanner() {
+    if (video) {
+        video.pause();
+        video.srcObject.getTracks()[0].stop();
+        scanning = false;
+    }
+}
+
+function tick() {
+    if (video.readyState === video.HAVE_ENOUGH_DATA) {
+        canvasElement.height = video.videoHeight;
+        canvasElement.width = video.videoWidth;
+        canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+        const imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
+        const code = jsQR(imageData.data, imageData.width, imageData.height, {
+            inversionAttempts: "dontInvert",
+        });
+        if (code) {
+            resultContainer.innerHTML = code.data;
+        }
+    }
+    requestAnimationFrame(tick);
+}
+
+startButton.addEventListener("click", startScanner);
+stopButton.addEventListener("click", stopScanner);
